98ae368bcea31839679e0917188c0589000eee72
Alice Moraes <alicefms@hotmail.com>
2021-06-27 21:22:26 -0300

    refatora calculadora, lê valor inserido, e exibe relatorio no console

--- calculadora.js
+++ calculadora.js
@@ -1,15 +1,12 @@
 let reais = 15
+let views = 0
 let clicks = 0
 let compartilhamentos = 0
 let maisviews = 0
+let viewsTotais = 0
 let compaTotais = 0
 let clicksTotais = 0
 let share = 0
-
-let views = 30 * reais
-
-let viewsTotais = views
-
 
 function calculadora(view) {
 
@@ -23,29 +20,56 @@ function calculadora(view) {
     return maisviews
 }
 
-do {
-    if (share < 3) {
-        views = calculadora(views)
-    }
+function geraRelatorio (valor){
+    views = 30 * valor
+    viewsTotais = views
 
-    share = share + 1
+    do {
+        if (share < 3) {
+            views = calculadora(views)
+        }
+    
+        share = share + 1
+    
+        if (share == 3) break
+    } while (compartilhamentos >= 1)
+    
+    clicksTotais = clicksTotais + Math.floor(12 / 100 * views)
+}
 
-    if (share == 3) break
-} while (compartilhamentos >= 1)
 
-clicksTotais = clicksTotais + Math.floor(12 / 100 * views)
 
-console.log(` 
+var form = document.getElementById('cadastro')
+var invest = document.querySelector('.test')
+
+form.addEventListener('submit', function (e) {
+    
+    e.preventDefault();
+    reais = invest.value
+   geraRelatorio(reais)
+
+   console.log(` 
 Valor Investido = ${reais}
 Total de Views = ${viewsTotais}
 Total de Clicks = ${clicksTotais}
 Total de Compartilhamentos = ${compaTotais}
 `)
+})
 
 
-document.getElementById('reais').innerHTML = `${reais}`
-document.getElementById('viewsTotais').innerHTML = `${viewsTotais}`
-document.getElementById('clicksTotais').innerHTML = `${clicksTotais}`
-document.getElementById('compaTotais').innerHTML = `${compaTotais}`
 
 
+
+
+
+
+
+
+
+// document.getElementById('reais').innerHTML = `${reais}`
+// document.getElementById('viewsTotais').innerHTML = `${viewsTotais}`
+// document.getElementById('clicksTotais').innerHTML = `${clicksTotais}`
+// document.getElementById('compaTotais').innerHTML = `${compaTotais}`
+
+
+ 
